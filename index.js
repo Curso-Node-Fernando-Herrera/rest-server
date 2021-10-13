625eb50d1a0f1072c5e4b6c8b870143256b3fd4a
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-13 19:40:58 -0300

    apply routers and controllers

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const cors = require('cors')
+
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send('Hola mundo')
-})
+// CORS
+app.use(cors())
+// get static content from public folder
+app.use(express.static('public'))
+// leer y parcear json que viene del body de mis post, etc
+app.use(express.json())
+
+// routers
+app.use('/api/users', require('./routes/users.routes'))
 
 app.listen(port, () => {
   console.log(`Connecting for http://localhost:${port}`)
