2ac492654dafc2b944d6172efbae2b255f0fb637
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-15 21:54:16 -0300

    change all controllers

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,26 +1,57 @@
-const getUsers = (req, res) => {
-  const { query } = req
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
+
+const getUsers = async (req, res) => {
+  const { limit = 10, offset = 0 } = req.query
+  const EXIST_USERS = { state: true }
+
+  const [totalUsers, users] = await Promise.all([
+    await User.count(EXIST_USERS),
+    await User.find(EXIST_USERS).limit(Number(limit)).skip(Number(offset)),
+  ])
+
   res.json({
-    displayName: 'Mateoac12',
-    avatar: null,
-    ...query,
+    totalUsers,
+    users,
   })
 }
 
-const postUsers = (req, res) => {
+const postUsers = async (req, res) => {
   const { body } = req
-  const { displayName, nickName } = body
+  const { displayName, email, password, avatar, role, state, google } = body
+
+  const salt = bcrypt.genSaltSync(10)
+  const passwordHash = bcrypt.hashSync(password, salt)
+
+  const user = new User({
+    displayName,
+    email,
+    password: passwordHash,
+    avatar,
+    role,
+    state,
+    google,
+  })
+
+  await user.save()
 
   res.json({
-    displayName,
-    nickName,
+    user,
   })
 }
 
-const putUsers = (req, res) => {
+const putUsers = async (req, res) => {
+  const { id } = req.params
+
+  const { displayName, password } = req.body
+
+  const user = await User.findByIdAndUpdate(id, {
+    displayName,
+    password,
+  })
+
   res.json({
-    newUser: 'madeval',
-    newAvatar: null,
+    user,
   })
 }
 
@@ -31,10 +62,13 @@ const patchUsers = (req, res) => {
   })
 }
 
-const deleteUsers = (req, res) => {
+const deleteUsers = async (req, res) => {
+  const { id } = req.params
+
+  const user = await User.findByIdAndUpdate(id, { state: false })
+
   res.json({
-    newUser: 'madeval',
-    newAvatar: null,
+    user,
   })
 }
 
