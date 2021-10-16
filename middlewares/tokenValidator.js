ccee86846afaad9a1efc204e5781552dbccb731b
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-16 19:31:58 -0300

    new middleware for tokens

---
+++ middlewares/tokenValidator.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const tokenValidator = async (req, res, next) => {
+  const token = req.header('x-token')
+
+  if (!token) {
+    return res.status(401).json({
+      error: 'Token is missing',
+    })
+  }
+
+  try {
+    const { id: uid } = jwt.verify(token, process.env.JWT_KEY)
+    const userFound = await User.findById(uid)
+    console.log(userFound)
+    if (!userFound) {
+      return res.status(401).json({
+        error: 'User not exist in database',
+      })
+    }
+
+    if (!userFound.state) {
+      return res.status(401).json({
+        error: 'User was deleted',
+      })
+    }
+
+    next()
+  } catch (error) {
+    console.error(error)
+    res.status(401).json({
+      error: 'Invalid token',
+    })
+  }
+}
+
+module.exports = {
+  tokenValidator,
+}
