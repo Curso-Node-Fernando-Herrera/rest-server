d75c4909b572e640708b1a8b24d1167a051cd247
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-17 20:52:07 -0300

    model of categories

---
+++ models/category.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose')
+
+const CategorySchema = Schema({
+  name: {
+    type: String,
+    required: [true, 'Category name is required'],
+    unique: true,
+  },
+  state: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'need user to approve create a category'],
+  },
+})
+
+CategorySchema.methods.toJSON = function () {
+  const { __v, _id: id, ...categoryData } = this.toObject()
+
+  return {
+    ...categoryData,
+    id,
+  }
+}
+
+module.exports = model('Category', CategorySchema)
