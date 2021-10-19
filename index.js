1bb167272065404c00cf7ee45e8511ae73af24ab
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-18 21:41:32 -0300

    add search route

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ const { dbConection } = require('./database/config')
 
 const app = express()
 dbConection()
+
 const port = process.env.PORT
 
 // CORS
 app.use(cors())
+
 // get static content from public folder
 app.use(express.static('public'))
+
 // leer y parcear json que viene del body de mis post, etc
 app.use(express.json())
 
 // routers
 app.use('/auth', require('./routes/auths.routes'))
+app.use('/api/search', require('./routes/search.routes'))
 app.use('/api/users', require('./routes/users.routes'))
 app.use('/api/categories', require('./routes/categories.routes'))
 app.use('/api/products', require('./routes/products.routes'))
